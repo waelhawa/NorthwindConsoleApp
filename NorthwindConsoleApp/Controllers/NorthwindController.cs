2b6cee755d20f6348a0d28e21ba3243c62006b2c
waelhawa <wael.hawa86@gmail.com>
2021-07-08 11:04:11 -0400

    First Commit

---
+++ NorthwindConsoleApp/Controllers/NorthwindController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NorthwindConsoleApp.Models;
+
+namespace NorthwindConsoleApp.Controllers
+{
+    public class NorthwindController : Controller
+    {
+        private readonly NorthwindContext _context;
+
+        public NorthwindController(NorthwindContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var customers = _context.Employees.ToList();
+            return View(customers);
+        }
+    }
+}
